de86e59bef29f098fbbea1499054475b7b8c0ec4
Rohan George D <rohan.geo.2001@gmail.com>
2020-07-06 15:55:57 +0530

    Updates

---
+++ precache-manifest.b77a108569f3590b30f4e7b779e29529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ebbb414f5479e38fcda7704ea51d5dd",
+    "url": "/React-CRUD/index.html"
+  },
+  {
+    "revision": "3344d89891155f1795be",
+    "url": "/React-CRUD/static/css/main.a9b8aa7f.chunk.css"
+  },
+  {
+    "revision": "cd9707e8147c8b531c20",
+    "url": "/React-CRUD/static/js/2.bd658685.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-CRUD/static/js/2.bd658685.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3344d89891155f1795be",
+    "url": "/React-CRUD/static/js/main.b480ecd3.chunk.js"
+  },
+  {
+    "revision": "ba851e470060786932d9",
+    "url": "/React-CRUD/static/js/runtime-main.846deacb.js"
+  }
+]);
